beb66157504fe78189eddaa88d44d7e9e5b5171a
facurie <facundorn3@gmail.com>
2026-01-14 13:45:34 -0500

    adding amount function

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,18 @@ function calculateArea() {
 
  document.getElementById("result").innerText = `The area of the rectangle is ${area}`;
 }
+
+
+function calculateAmount() {
+
+
+    let groc1 = parseFloat(document.getElementById("groc1").value);
+    let groc2 = parseFloat(document.getElementById("groc2").value);
+    let groc3 = parseFloat(document.getElementById("groc3").value);
+
+    let amount = groc1 + groc2 + groc3;
+
+    document.getElementById("res1").innerText = `The total amount is: $${amount}`;
+
+
+}
